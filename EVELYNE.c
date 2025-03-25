899feb84b5dacdc8752e320334c7c71ea1a04ab0
Evelyne2244 <seruggaevelyne80@gmail.com>
2025-03-25 09:41:06 +0300

    add  evelyne.c

---
+++ EVELYNE.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int balance=50000;
+    int withdraw;
+    printf("enter amount to withdraw");
+    scanf("%d" ,&withdraw);
+    if(withdraw>balance){
+        printf("insufficient balance.current balance %d\n",balance);
+    }
+    else{
+        int account_balance;
+        account_balance=balance-withdraw;
+        printf("remaining balance:%d\n",account_balance);
+    }
+    return 0;
+}
+
+
+    
---
+++ nestedif.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main {
+    int x =10, y = 5;
+    if(x>5){
+        if(y>4){
+            printf("both conditions are true\n");
+        }
+    }
+    return 0;
+}
